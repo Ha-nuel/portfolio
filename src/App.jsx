c86d66dd129524a387e9884c7f61933637dff9ca
이하늘 <wsxedc901@gmail.com>
2023-03-12 21:13:42 +0900

    App.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import Header from './components/UI/Header';
 import About from './pages/About';
 import Footer from './components/UI/Footer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
-      <Routes>
-        <Route path='/' element={<About />} />
-      </Routes>
+      <About />
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
